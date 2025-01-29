b91d2edf6c5dac39f0194dfda63368ea0f931095
Sparrow <wroblewski.m@int.pl>
2025-01-29 13:43:28 +0100

    Lab2Polecenie10 Gotowe

---
+++ A.Podolski/Lab2Polecenie10/Program.cs
@@ -0,0 +1,9 @@
+﻿Random liczba = new Random();
+Int32 a, b;
+
+for (b=0; b<=20; b++)
+{
+    a = liczba.Next(10,20); // Next przekazuje parametry do metody Random(), zakres losowanych liczb to 10-19, bo 20 nie jest wliczane
+    Console.WriteLine(a);
+}
+Console.ReadKey();
