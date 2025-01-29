aeb08f3cf3c0d47fd17e9320e5ff281e493d6db3
Sparrow <wroblewski.m@int.pl>
2025-01-29 22:33:25 +0100

    Lab3 Polecenie 4 Gotowe

--- A.Podolski/Lab3Polecenie4/Program.cs
+++ A.Podolski/Lab3Polecenie4/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[,] tab1 = new int[4, 6];
+        Random rand = new Random();
+
+        // Wypełnienie tablicy losowymi liczbami z przedziału 100-999
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                tab1[i, j] = rand.Next(100, 1000);
+            }
+        }
+
+        // Wyświetlenie tablicy na ekranie
+        Console.WriteLine("Tablica 4x6:");
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                Console.Write(tab1[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
