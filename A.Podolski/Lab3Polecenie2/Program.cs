84871a88656782b62aa3868d7904b245ea664ce3
Sparrow <wroblewski.m@int.pl>
2025-01-29 20:46:48 +0100

    Lab3Polecenie 2 Gotowe

---
+++ A.Podolski/Lab3Polecenie2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] tab1 = new int[10];
+
+        // Wypełnienie tablicy liczbami od 9 do 0
+        for (int i = 0; i < tab1.Length; i++)
+        {
+            tab1[i] = 9 - i;
+        }
+
+        // Wyświetlenie tablicy na ekranie
+        Console.WriteLine("Indeks | Wartość");
+        Console.WriteLine("----------------");
+        for (int i = 0; i < tab1.Length; i++)
+        {
+            Console.WriteLine($"   {i}   |    {tab1[i]}");
+        }
+    }
+}
