14176750c56061790dca7a85c3fca3c8c2089440
Sparrow <wroblewski.m@int.pl>
2025-01-29 13:11:06 +0100

    Lab 2 Polecenie 5 Gotowe

---
+++ A.Podolski/Lab2Polecenie5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Pętla for dla x zmieniającego się w przedziale od 1 do 20
+        for (int x = 1; x <= 20; x++)   // wartość początkowa | warunek | inkrementacja (x = x + 1)
+        {
+            // Obliczanie wartości funkcji y = 2x + 1
+            int y = 2 * x + 1;
+
+            // Wyświetlanie wyniku
+            Console.WriteLine($"Dla x = {x}, y = {y}");
+        }
+
+        // Zgłoszenie zakończenia realizacji polecenia
+        Console.WriteLine("\nZakończenie realizacji polecenia.");
+    }
+}
