c38734503dba4840651a77f4f7ecd67c8fb73561
Sparrow <wroblewski.m@int.pl>
2025-01-29 23:01:40 +0100

    Lab3 Polecenie 6 Gotowe

--- A.Podolski/Lab3Polecenie6/Program.cs
+++ A.Podolski/Lab3Polecenie6/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[,] tab1 = new int[10, 10];
+        Random rand = new Random();
+
+        // Wypełnienie tablicy losowymi liczbami z przedziału 0-999
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                tab1[i, j] = rand.Next(0, 1000);
+            }
+        }
+
+        // Wyświetlenie tablicy z wyrównaniem kolumn
+        Console.WriteLine("Tablica 10x10:");
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                Console.Write(tab1[i, j].ToString().PadLeft(4) + " "); //Każda liczba ma zarezerwowane 4 znaki szerokości
+            }
+            Console.WriteLine();
+        }
+    }
+}
