6016fc0aa921f0b3e4bb28838cec2e99a4364fcb
Sparrow <wroblewski.m@int.pl>
2025-01-29 13:21:22 +0100

    Lab2Polecenie6 Gotowe

---
+++ A.Podolski/Lab2Polecenie6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int x = 1; // Początkowa wartość x
+
+        // Pętla do...while
+        do
+        {
+            int y = 2 * x + 1; // Obliczenie wartości funkcji y = 2x + 1
+            Console.WriteLine($"Dla x = {x}, y = {y}");
+            x++; // Zwiększenie wartości x
+        }
+        while (x <= 20); // Warunek zakończenia pętli (x zmienia się od 1 do 20)
+
+        // Zgłoszenie zakończenia realizacji polecenia
+        Console.WriteLine("Zakończenie realizacji polecenia.");
+    }
+}
