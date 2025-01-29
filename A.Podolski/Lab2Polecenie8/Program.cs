f01c11ccb95e5fb07036fca3b9aec2d01c25e00a
Sparrow <wroblewski.m@int.pl>
2025-01-29 13:32:37 +0100

    Lab2 Polecenie 8 Gotowe

---
+++ A.Podolski/Lab2Polecenie8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int x = 1; // Początkowa wartość x
+
+        // Pętla while
+        while (x <= 20)
+        {
+            int y = 2 * x + 1; // Obliczenie wartości funkcji y = 2x + 1
+            Console.WriteLine($"Dla x = {x}, y = {y}");
+            x++; // Zwiększenie wartości x
+        }
+
+        // Zgłoszenie zakończenia realizacji polecenia
+        Console.WriteLine("Zakończenie realizacji polecenia.");
+    }
+}
+// W pentli WHILE najpierw jest sprawdzony warunek i dopiero zapada decyzja czy wykonywać program czy nie, a w pentli DO WHILE najpierw wykonywany jest kod, a później następuje sprawdzenie warunku, jeśli jest prawdziwy to kod wykonywany jest ponownie, zapewnia to że kod zostanie wykonany przynajmniej 1 raz nawet jeśli warunek nie jest spełniony 
