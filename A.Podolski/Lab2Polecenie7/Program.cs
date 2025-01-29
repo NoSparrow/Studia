b94c469d004c627ce00065774d4b0268213e6e28
Sparrow <wroblewski.m@int.pl>
2025-01-29 13:26:21 +0100

    Lab2Polecenie7 Gotowe

---
+++ A.Podolski/Lab2Polecenie7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int x = 1; // Początkowa wartość x
+
+        // Pętla do...while
+        do
+        {
+            x++; // Inkrementacja przeniesiona na początek
+
+            int y = 2 * x + 1; // Obliczenie wartości funkcji y = 2x + 1
+            Console.WriteLine($"Dla x = {x}, y = {y}");
+        }
+        while (x < 20); // Warunek zakończenia pętli (x zmienia się od 1 do 20)
+
+        // Zgłoszenie zakończenia realizacji polecenia
+        Console.WriteLine("Zakończenie realizacji polecenia.");
+    }
+}
+// Lokalizacja zapisu ma wpływ na działanie pętli, zmieniając miejsce inkrementacji zmienia się kolejność wykonywanych działań, tutaj najpierw zostanie wykonana inkrementacja x++ i dopiero później wyliczenie y. Skutkuje to pominięciem wartości x = 1.
