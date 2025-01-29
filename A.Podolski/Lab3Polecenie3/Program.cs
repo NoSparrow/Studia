6ad41b806a194d81cc6da2374cedeb9747330147
Sparrow <wroblewski.m@int.pl>
2025-01-29 22:09:25 +0100

    Lab3 Polecenie 3 Gotowe

---
+++ A.Podolski/Lab3Polecenie3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int countA = 0;
+        int countB = 0;
+
+        // Pętla A (zewnętrzna)
+        for (int i = 0; i < 5; i++)
+        {
+            countA++;
+            Console.WriteLine($"Pętla A - Iteracja {i}");
+
+            // Pętla B (wewnętrzna)
+            for (int j = 0; j < 4; j++)
+            {
+                countB++;
+                Console.WriteLine($"   Pętla B - Iteracja {j}");
+            }
+        }
+
+        Console.WriteLine($"Pętla A wykonała się {countA} razy.");
+        Console.WriteLine($"Pętla B wykonała się {countB} razy.");
+    }
+}
