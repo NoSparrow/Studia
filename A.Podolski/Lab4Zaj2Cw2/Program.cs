e24e89b30852cbd64a9e752d89b264f0209fa4b3
Sparrow <wroblewski.m@int.pl>
2025-01-31 16:32:23 +0100

    Lab2 Zadanie 2

---
+++ A.Podolski/Lab4Zaj2Cw2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Podaj liczbę N: ");
+        int N = int.Parse(Console.ReadLine());
+        int suma = 0;
+        
+        for (int i = 1; i <= N; i++)
+        {
+            suma += i;
+        }
+        
+        Console.WriteLine("Suma liczb od 1 do " + N + " wynosi: " + suma);
+    }
+}
